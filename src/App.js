693f90e2cdfe661bc2ffc6090936b490f0e19698
unknown <portybed@gmail.com>
2020-04-05 23:33:26 +0300

    task 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import DishDetail from './components/DishdetailComponent';
 import './App.css';
 import { DISHES } from './shared/dishes';
-import { render } from '@testing-library/react';
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      selectedDishID: null
     };
   }
+  
+  onDishSelect(ID){
+    this.setState({selectedDishID: ID})
+  } 
   render() {
       return (
       <div>
@@ -22,7 +25,8 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDishID)[0]} />
       </div>
     );
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -10,6 +10,11 @@ class Menu extends Component {
         this.state = {
             selectedDish: null
         }
+        console.log('Menu Component constructor is invoked');
+    }
+
+    componentDidMount() {
+        console.log('Menu Component componentDidMount is invoked');
     }
 
     onDishSelect(dish) {
@@ -33,12 +38,14 @@ class Menu extends Component {
             );
     }
 
+   
+
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div  className="col-12 col-md-5 m-1">
                 <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
+                  onClick={() => this.props.onClick(dish.id)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
@@ -48,16 +55,21 @@ class Menu extends Component {
             );
         });
 
+        console.log('Menu Component render is invoked');
+
         return (
             <div className="container">
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
+                {/*<div className="row">
+                  <div className="col-12 col-md-5 m-1">
                     {this.renderDish(this.state.selectedDish)}
                   </div>
                 </div>
+                <div className="row">
+                    {this.renderDishComments(this.state.selectedDish)}
+        </div>*/}
             </div>
         );
     }
