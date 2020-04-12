38f778d9077600d363418b258b48ee95c16c9487
unknown <portybed@gmail.com>
2020-04-12 19:19:30 +0300

    Single Page Application Part2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+import { Card, CardImg, CardImgOverlay, Breadcrumb, BreadcrumbItem,
     CardTitle } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 function RenderMenuItem({dish, onClick}) {
     return(
-        <Card key={dish.id}>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
                     </CardImgOverlay>
-                    </Card>
+                    
+            </Link>
+        </Card>
+                    
     );
 }
   
@@ -19,7 +24,7 @@ function RenderMenuItem({dish, onClick}) {
     const menu = props.dishes.map((dish) => {
         return (
           <div  className="col-12 col-md-5 m-1">
-            <RenderMenuItem dish={dish} onClick={props.onClick} />
+            <RenderMenuItem dish={dish} />
           </div>
         );
     });
@@ -27,6 +32,16 @@ function RenderMenuItem({dish, onClick}) {
 
     return (
         <div className="container">
+            <div classname="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>
+            </div>
             <div className="row">
                 {menu}
             </div>
